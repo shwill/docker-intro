4b71a6e4e560d2e30a493200a87969262b9cac40
Sebastian Will <sebastian.h.will@gmail.com>
2022-06-14 15:41:24 +0200

    Trying to figure out verify

--- course1/step4/verify.sh
+++ course1/step4/verify.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker image ls --filter 'reference=myapp:1.0.0' --format "{{.ID}}"
+if [[ $(docker image ls --filter 'reference=myapp:1.0.0' --format "{{.ID}}") ]]; then
+    exit 0
+else
+    exit 1
+fi
